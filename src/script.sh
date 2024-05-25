084724b8be5262c37967f0370e5b3661e652d842
sabaly <tmssam47@gmail.com>
2024-05-25 21:23:10 +0200

    new stats

--- src/script.sh
+++ src/script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-nb_teachers="10 30 45"
+nb_teachers="45"
 for nb_tchrs in $nb_teachers
 do
     echo ">>> Number of teachers : " $nb_teachers
-    for nb_fair in $(seq 1 $(($nb_tchrs/2)))
+    for nb_fair in $(seq 10 $(($nb_tchrs/2)))
     do
         python3 fairness_impact_eval.py $nb_tchrs $nb_fair
     done
